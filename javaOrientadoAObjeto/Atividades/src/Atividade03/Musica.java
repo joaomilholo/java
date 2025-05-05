d5d40986944a13d0328338ee4afda68373bf08ae
Joao Milholo <joaomilholo12@gmail.com>
2025-05-05 14:25:59 -0300

    atividade 03

--- javaOrientadoAObjeto/Atividades/src/Atividade03/Musica.java
+++ javaOrientadoAObjeto/Atividades/src/Atividade03/Musica.java
@@ -1,17 +1,29 @@
 package Atividade03;
 
 public class Musica {
-    String titulo;
-    String artista;
-    int anoLancamento;
-    double avaliacao;
-    int numAvaliacoes;
+    public String titulo;
+    public String artista;
+    public int anoLancamento;
+    public double avaliacao;
+    public int numAvaliacoes;
 
     public void exibirFichaTecnica() {
-        System.out.println("-------- Ficha Tecnica --------");
+        System.out.println("-------- Ficha Técnica --------");
         System.out.println("Nome: " + titulo);
         System.out.println("Artista: " + artista);
         System.out.println("Ano de lançamento: " + anoLancamento);
+        System.out.println("Total de avaliações: " + numAvaliacoes);
+        System.out.println("Média das avaliaçoes: " + String.format("%.2f",obterMedia()));
+        System.out.println("-------------------------------");
+    }
+
+    public void avalia(double nota) {
+        avaliacao += nota;
+        numAvaliacoes++;
+    }
+
+    public double obterMedia() {
+        return avaliacao / numAvaliacoes;
     }
 
 }
--- javaOrientadoAObjeto/Atividades/src/Main.java
+++ javaOrientadoAObjeto/Atividades/src/Main.java
@@ -15,6 +15,12 @@ public class Main {
 
         // Atividade 3
         Musica musica = new Musica();
-
+        musica.titulo = "Postmortem";
+        musica.artista = "Slayer";
+        musica.anoLancamento = 1986;
+        musica.avalia(10);
+        musica.avalia(8.5);
+        musica.avalia(5.6);
+        musica.exibirFichaTecnica();
     }
 }
